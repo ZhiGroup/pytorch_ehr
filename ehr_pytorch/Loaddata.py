886cecdbe30a43b617d3d8660439cef31cfab7e9
chocolocked <GinnyChu512@gmail.com>
2018-04-30 14:21:28 -0500

    Update Loaddata.py

--- ehr_pytorch/Loaddata.py
+++ ehr_pytorch/Loaddata.py
@@ -11,7 +11,7 @@ import random
 merged_set takes in the format of labeled_ehr_seq_list,
 for each patient visit is either a list for LG
 or list of list for NN models
-To do: 1. future load_data might take in train and test data from seperate files 
+To do: 1. future load_data might take in train and test data from seperate files (done in main file)
        2. future load_data might take in time sequence component
 """
 def load_data(merged_set, test_ratio = 0.2, validation_ratio = 0.1):  
