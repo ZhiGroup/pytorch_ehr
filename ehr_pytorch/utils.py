ff06c8a48e63a7826bcd3f4adf46e330291fa9fa
Xin Hao <42793737+Xin128@users.noreply.github.com>
2019-06-14 09:52:06 -0500

    Update utils.py

--- ehr_pytorch/utils.py
+++ ehr_pytorch/utils.py
@@ -11,7 +11,7 @@ from __future__ import print_function, division
 from io import open
 #import string
 #import re
-#import random
+#import random   ##################### XIN: DO WE NEED TO IMPORT THESE??????????????????????????????????????????
 import math 
 import time 
 import os
@@ -31,19 +31,19 @@ import matplotlib.pyplot as plt
 #import matplotlib.ticker as ticker
 import numpy as np
 
-#Rename afterwards
+
 import EHRDataloader
 from EHRDataloader import iter_batch2
 #silly ones
 from termcolor import colored
 
 
-#check this later
+
 use_cuda = torch.cuda.is_available()
 #device = torch.device("cuda:0" if use_cuda else "cpu")
 #cudnn.benchmark = True
 
-###### minor functions, plots and prints
+
 #loss plot
 def showPlot(points):
     fig, ax = plt.subplots()
@@ -89,7 +89,7 @@ def trainsample(sample, model, optimizer, criterion = nn.BCELoss()):
 
 #train with loaders
 
-def trainbatches(loader, model, optimizer, shuffle = True):#,we dont need this print print_every = 10, plot_every = 5): 
+def trainbatches(loader, model, optimizer, shuffle = True, which_model = 'RNN'):#,we dont need this print print_every = 10, plot_every = 5): 
     current_loss = 0
     all_losses =[]
     plot_every = 5
@@ -99,14 +99,18 @@ def trainbatches(loader, model, optimizer, shuffle = True):#,we dont need this p
          loader = iter_batch2(loader, len(loader))
     for i,batch in enumerate(loader):
         #batch.to(device) #see if it works
-        output, loss = trainsample(batch, model, optimizer, criterion = nn.BCELoss())
+        if which_model == "LR":
+            for pt in batch:
+                out,loss =  trainsample(pt, model, optimizer, criterion = nn.BCELoss())
+            output.append(out.cpu().data.numpy()[0][0])
+        else:
+            output, loss = trainsample(batch, model, optimizer, criterion = nn.BCELoss())
         current_loss += loss
         n_iter +=1
     
         if n_iter % plot_every == 0:
             all_losses.append(current_loss/plot_every)
             current_loss = 0    
-        #print(all_losses) #check    
     return current_loss, all_losses 
 
 
@@ -120,20 +124,22 @@ def calculate_auc(model, loader, which_model = 'RNN', shuffle = True): # batch_s
     for i,batch in enumerate(loader):
             #batch.to(device) #check you want it or not 
             output, label_tensor = model(batch)
+            # Normal cases
             if which_model != 'LR':
                 y_hat.extend(output.cpu().data.view(-1).numpy())  
                 y_real.extend(label_tensor.cpu().data.view(-1).numpy())
          
             else: 
-                #The minor case, basically embedding LR and GRU-LR case. Do we want to keep it?
-                y_hat.append(output.cpu().data.numpy()[0][0])
-                y_real.append(label_tensor.cpu().data.numpy()[0][0])
+                # Minor case:LR Model
+                for pt in batch:
+                    out,label_tensor =  model(pt)
+                    y_hat.append(out.cpu().data.numpy()[0][0])
+                    y_real.append(label_tensor.cpu().data.numpy()[0][0])
     
     auc = roc_auc_score(y_real, y_hat)
     return auc, y_real, y_hat 
 
-    
-#define the final epochs running, use the different names
+   
 
 def epochs_run(epochs, train, valid, test, model, optimizer, shuffle = True, which_model = 'RNN', patience = 20, output_dir = '../models/', model_prefix = 'hf.train', model_customed= ''):  
     bestValidAuc = 0.0
@@ -144,7 +150,7 @@ def epochs_run(epochs, train, valid, test, model, optimizer, shuffle = True, whi
     #print2file(header, logFile)
     for ep in range(epochs):
         start = time.time()
-        current_loss, train_loss = trainbatches(loader = train, model= model, optimizer = optimizer)
+        current_loss, train_loss = trainbatches(loader = train, model= model, optimizer = optimizer, which_model = which_model)
 
         train_time = timeSince(start)
         #epoch_loss.append(train_loss)
