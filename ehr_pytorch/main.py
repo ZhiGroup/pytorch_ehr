e4855afd9228fb55211083d8f090bbb4cacf2ed0
lrasmy <laila.rasmy@consultant.com>
2019-11-06 13:26:22 -0600

    ## LR 11/6/19
    Solving  reported issue

--- ehr_pytorch/main.py
+++ ehr_pytorch/main.py
@@ -74,7 +74,7 @@ def main():
     parser.add_argument('-batch_size', type=int, default=128, help='batch size for training, validation or test [default: 128]')
     #EHRmodel
     parser.add_argument('-which_model', type = str, default = 'DRNN',choices= ['RNN','DRNN','QRNN','TLSTM','LR','RETAIN'], help='choose from {"RNN","DRNN","QRNN","TLSTM","LR","RETAIN"}') 
-    parser.add_argument('-cell_type', type = str, default = 'GRU', choices=['RNN', 'GRU', 'LSTM', 'QRNN','TLSTM' ], help='For RNN based models, choose from {"RNN", "GRU", "LSTM", "QRNN" (for QRNN model only)}, "TLSTM (for TLSTM model only')
+    parser.add_argument('-cell_type', type = str, default = 'GRU', choices=['RNN', 'GRU', 'LSTM'], help='For RNN based models, choose from {"RNN", "GRU", "LSTM")  ## LR 11/6/19
     ####Think about whether you want to keep this RNN or LR based, or just call all different models
     parser.add_argument('-input_size', nargs='+', type=int , default = [15817], help='''input dimension(s) separated in space the output will be a list, decide which embedding types to use. 
                         If len of 1, then  1 embedding; len of 3, embedding medical, diagnosis and others separately (3 embeddings) [default:[15817]]''')
