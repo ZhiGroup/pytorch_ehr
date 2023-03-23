d938f3255596d1404cba7a50fab8ac033e742c8b
Laila Rasmy <laila.rasmy@consultant.com>
2023-03-23 13:46:11 -0500

    Bidirectional

--- Pytorch_EHR_Tutorial/Model_Training/pytorch_ehr/explain_ig.py
+++ Pytorch_EHR_Tutorial/Model_Training/pytorch_ehr/explain_ig.py
@@ -11,7 +11,7 @@ import numpy as np
 from torch.autograd import Variable
 import torch.nn.functional as F
 from tqdm import tqdm
-import pickle as pk
+import pickle5 as pk
 import pandas as pd
 import random
 use_cuda = torch.cuda.is_available()
@@ -49,15 +49,22 @@ def EmbedPatients_MB_explain(bmodel,mb_t, mtd): #let's define this
     return out_emb, embedded_1
 
 
-def int_grad_pt(best_model , diag_t, code_desc, pt , m=64 , qc=False):
+def int_grad_pt(best_model , diag_t, code_desc, pt , bi=False, m=64 , qc=False):
     x1, label,seq_len,time_diff = pt
     with torch.no_grad(): output_score = best_model(x1,seq_len,time_diff)
     x_in,x_in2= EmbedPatients_MB_explain(best_model,x1,time_diff)
     code_visit_cont = torch.zeros_like(x_in2)
     best_model.train()### LR added for error
-    if best_model.time==True:
-        f = lambda x, x1: best_model.sigmoid(best_model.out(best_model.rnn_c(torch.cat((x.sum(2), x1[...,-1:]), dim=2))[1][-1])).squeeze()
-    else: f = lambda x, x1: best_model.sigmoid(best_model.out(best_model.rnn_c(x.sum(2))[1][-1])).squeeze()
+    if bi==False:
+        if best_model.time==True:
+            f = lambda x, x1: best_model.sigmoid(best_model.out(best_model.rnn_c(torch.cat((x.sum(2), x1[...,-1:]), dim=2))[1][-1])).squeeze()
+        else: f = lambda x, x1: best_model.sigmoid(best_model.out(best_model.rnn_c(x.sum(2))[1][-1])).squeeze()
+    else:
+        if best_model.time==True:
+            f = lambda x, x1: best_model.sigmoid(best_model.out(torch.cat((best_model.rnn_c(torch.cat((x.sum(2), x1[...,-1:]), dim=2))[1][-1],best_model.rnn_c(torch.cat((x.sum(2), x1[...,-1:]), dim=2))[1][-2]),1))).squeeze()
+
+        else: f = lambda x, x1: best_model.sigmoid(best_model.out(torch.cat((best_model.rnn_c(x.sum(2))[1][-1],best_model.rnn_c(x.sum(2))[1][-2]),1))).squeeze()
+
     for k in range(1, m):
         x_in2.grad = None
         pred = f(x_in2/m*k, x_in)
@@ -100,6 +107,64 @@ def int_grad_pt(best_model , diag_t, code_desc, pt , m=64 , qc=False):
     
     return label, output_score ,pt_inp_cont[pt_inp_cont['TOKEN_ID']>0]
 
+def int_grad_pt_nodesc (best_model , diag_t, pt , bi=False, m=64 , qc=False):
+    x1, label,seq_len,time_diff = pt
+    with torch.no_grad(): output_score = best_model(x1,seq_len,time_diff)
+    x_in,x_in2= EmbedPatients_MB_explain(best_model,x1,time_diff)
+    code_visit_cont = torch.zeros_like(x_in2)
+    best_model.train()### LR added for error
+    if bi==False:
+        if best_model.time==True:
+            f = lambda x, x1: best_model.sigmoid(best_model.out(best_model.rnn_c(torch.cat((x.sum(2), x1[...,-1:]), dim=2))[1][-1])).squeeze()
+        else: f = lambda x, x1: best_model.sigmoid(best_model.out(best_model.rnn_c(x.sum(2))[1][-1])).squeeze()
+    else:
+        if best_model.time==True:
+            f = lambda x, x1: best_model.sigmoid(best_model.out(torch.cat((best_model.rnn_c(torch.cat((x.sum(2), x1[...,-1:]), dim=2))[1][-1],best_model.rnn_c(torch.cat((x.sum(2), x1[...,-1:]), dim=2))[1][-2]),1))).squeeze()
+
+        else: f = lambda x, x1: best_model.sigmoid(best_model.out(torch.cat((best_model.rnn_c(x.sum(2))[1][-1],best_model.rnn_c(x.sum(2))[1][-2]),1))).squeeze()
+
+    for k in range(1, m):
+        x_in2.grad = None
+        pred = f(x_in2/m*k, x_in)
+        pred.backward()
+        code_visit_cont += x_in2.grad/k
+
+    best_model.eval()### LR added
+    code_visit_cont *= x_in2
+    ### QC check
+    if qc==True:
+        with torch.no_grad():
+            diff = f(x_in2, x_in) - f(x_in2/m, x_in)
+        print('output difference : ',diff, " , sum of contribution scores = ",code_visit_cont.sum())
+    
+    ### building DF for visualizations
+    if use_cuda: 
+            pt_inp=pd.DataFrame(x1.squeeze(0).cpu().numpy()).reset_index()
+    else:   pt_inp=pd.DataFrame(x1.squeeze(0).numpy()).reset_index()
+    pt_inp=pt_inp.melt(id_vars=['index'],value_vars=pt_inp.columns[1:])
+    pt_inp.columns=['visit_ind','token_pos','TOKEN_ID']
+    
+    if use_cuda: code_contrib=pd.DataFrame(code_visit_cont.sum(-1).cpu().detach().squeeze(0).numpy()).reset_index()
+    else: code_contrib=pd.DataFrame(code_visit_cont.sum(-1).detach().squeeze(0).numpy()).reset_index()
+    code_contrib=code_contrib.melt(id_vars=['index'],value_vars=code_contrib.columns[1:])
+    code_contrib.columns=['visit_ind','token_pos','cont_score']
+    if best_model.time==True:
+        with torch.no_grad(): 
+            #print(x_in[...,-1:].shape)
+            if x_in[...,-1:].shape[1]>1:
+                if use_cuda: timedf=pd.DataFrame(x_in[...,-1:].squeeze(0).cpu().numpy())
+                else: timedf=pd.DataFrame(x_in[...,-1:].squeeze(0).cpu().numpy())
+            else:timedf=pd.DataFrame([0])
+        timedf.columns=['Time']
+        pt_inp=pd.merge(pt_inp, timedf, how='left',left_on='visit_ind',right_on=timedf.index)
+    pt_inp = pd.merge(pt_inp,diag_t, how='left')
+    pt_inp_cont=pd.merge(pt_inp,code_contrib, how='left')#.dropna()
+    if best_model.time==True:
+        pt_inp_cont=pt_inp_cont[['visit_ind','Time','token_pos','TOKEN_ID','DIAGNOSIS_ID','cont_score']].drop_duplicates()
+    else: pt_inp_cont=pt_inp_cont[['visit_ind','token_pos','TOKEN_ID','DIAGNOSIS_ID','cont_score']].drop_duplicates()
+    
+    return label, output_score ,pt_inp_cont[pt_inp_cont['TOKEN_ID']>0]
+
 
 # In[3]:
 
@@ -118,8 +183,10 @@ def load_data(data_File,multilbl):
 	                              file = data_File, 
 	                              sort= True,
 	                              model='RNN')
+	#print(ds)
 	mbs_list = list(tqdm(EHRdataloader(ds, batch_size=1, packPadMode=True,multilbl=multilbl)))
-	return(mbs_list)
+	#print(len(mbs_list))
+	return(mbs_list)### mbs_list[:10] LR limiting to 10 for debugging
 
 # load the token dict
 def load_vocab_dict(type_File,desc_file):
@@ -129,6 +196,12 @@ def load_vocab_dict(type_File,desc_file):
 	code_desc=pd.read_table(desc_file)
 	code_desc.columns=['DIAGNOSIS_ID','cat','description']
 	return diag_t, code_desc
+
+def load_vocab_dict_nodesc(type_File):
+	tk_dc= pk.load(open(type_File,'rb'))
+	diag_t= pd.DataFrame.from_dict(tk_dc,orient='index').reset_index()
+	diag_t.columns=['DIAGNOSIS_ID','TOKEN_ID']
+	return diag_t
 
 
 # In[7]:
@@ -143,6 +216,32 @@ def calc_contribution (model_pth,data_File,type_File,desc_file,task='NA',qc=True
   l_df=[]
   for i , pt in enumerate(mbs_list):
     label, output_score , pt_inp_cont_1=int_grad_pt(best_model , diag_t, code_desc, pt , m=64 , qc=qc)
+    pt_inp_cont_1['PT']=i
+    if task=='mort':
+          pt_inp_cont_1['True_label']=label[:,:,0].squeeze().cpu().numpy()
+    elif task=='mort_surv':
+          pt_inp_cont_1['True_label']=label[:,:,0:2].squeeze().cpu().numpy()
+          pt_inp_cont_1['LOS']=label[:,:,1].squeeze().cpu().numpy()
+    elif task=='NA':
+          pt_inp_cont_1['True_label']=label.squeeze().cpu().numpy()
+    else: print ('for this tutorial task can be either mort or NA')
+	  
+    pt_inp_cont_1['predicted_score']=output_score.cpu().numpy()
+    l_df.append(pt_inp_cont_1)
+    df_f=pd.concat(l_df).sort_values(by=['PT','visit_ind'])
+  
+  return df_f
+
+def calc_contribution_nodesc (model_pth,data_File,type_File,task='NA',bi=True, qc=True):
+  if task == 'NA': multilbl=False
+  else:  multilbl=True
+  
+  best_model=load_model(model_pth)
+  mbs_list=load_data(data_File,multilbl=multilbl)
+  diag_t = load_vocab_dict_nodesc(type_File)
+  l_df=[]
+  for i , pt in enumerate(mbs_list):
+    label, output_score , pt_inp_cont_1=int_grad_pt_nodesc(best_model , diag_t, pt ,bi, m=64 , qc=qc)
     pt_inp_cont_1['PT']=i
     if task=='mort':
           pt_inp_cont_1['True_label']=label[:,:,0].squeeze().cpu().numpy()
